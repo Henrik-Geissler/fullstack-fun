247dfaa28daa486eb30c472bf6a3b35dbf78dfed
Your Name <you@example.com>
2020-11-30 19:38:46 +0100

    rm deploy.sh

--- server/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo What should the version be?
-read VERSION
-
-docker login
-docker build -t henrikgeissler/fullstackfun:$VERSION .
-docker push henrikgeissler/fullstackfun:$VERSION
-ssh root@104.248.251.75 "docker pull henrikgeissler/fullstackfun:$VERSION && docker tag henrikgeissler/fullstackfun:$VERSION dokku/api:$VERSION && dokku deploy api $VERSION"
